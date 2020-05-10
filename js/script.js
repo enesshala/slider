39deeef5862a9db1b338e0c694ac3de18ce76844
enesshala <51416158+enesshala@users.noreply.github.com>
2020-05-10 21:15:31 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-  });
+$(document).ready(function(){$('#autoWidth').lightSlider({autoWidth:true,loop:true,onSliderLoad:function(){$('#autoWidth').removeClass('cS-hidden');}});});
